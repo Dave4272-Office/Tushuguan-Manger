7b69c0d90476ff2a79ec15435cbc3a9bd34c734f
Debraj Kundu <kundudebraj4272@gmail.com>
2023-06-05 13:57:56 +0530

    requirements

---
+++ diag2md.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python ./kroki2md/kroki2md.py -i ./requirements/login.sequence.mmd -i ./requirements/logout.sequence.mmd -i ./requirements/interaction.sequence.mmd ./requirements/sequence.md
