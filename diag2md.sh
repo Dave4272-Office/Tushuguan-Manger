248a469d0983dd153e8a47e3e75c4a104ec9a867
Debraj Kundu <kundudebraj4272@gmail.com>
2023-12-03 03:24:35 +0530

    kroki util updates

--- diag2md.sh
+++ diag2md.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ./kroki2md/kroki2md.py -i ./requirements/login.sequence.mmd -i ./requirements/logout.sequence.mmd -i ./requirements/interaction.sequence.mmd ./requirements/sequence.md
+python ./kroki2md.py -i ./requirements/login.sequence.mmd -i ./requirements/logout.sequence.mmd -i ./requirements/interaction.sequence.mmd ./requirements/sequence.md
